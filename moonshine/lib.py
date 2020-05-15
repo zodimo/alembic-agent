8be06e4230d56e19b5dad6c25db5fd2d0b5f5c0a
zodimo <zodimo@gmail.com>
2020-05-15 15:35:49 +0200

    replace log with history and fix other issues

--- moonshine/lib.py
+++ moonshine/lib.py
@@ -1,13 +1,14 @@
 import logging
+
+logger = logging.getLogger(__name__)
 from alembic.runtime.environment import EnvironmentContext
 from alembic.script import ScriptDirectory
 from alembic.config import Config
+from alembic import util
 from sqlalchemy import engine_from_config, create_engine
 from sqlalchemy.engine import Engine
 import os, sys
-
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from contextlib import contextmanager
 
 
 class Moonshine:
@@ -29,13 +30,6 @@ class Moonshine:
             self.__engine = engine
         elif engine_config is not None:
             self.__engine = engine_from_config(engine_config)
-
-        # set defult values for script_location
-        script_location = "migrations"
-        self.config.set_main_option("script_location", script_location)
-        # set defult values for version_locations
-        version_locations = [script_location]
-        self.config.set_main_option("version_locations", ",".join(version_locations))
 
         # add logging handler if not configured
         console_handler = logging.StreamHandler(sys.stderr)
@@ -64,14 +58,26 @@ class Moonshine:
     def config(self) -> Config:
         if isinstance(self.__config, Config):
             return self.__config
+
         self.__config = Config()
+        # set defaults for script_location and version_locations
+        current_dir_path = os.path.dirname(os.path.realpath(__file__))
+        script_location = os.path.join(current_dir_path, "migrations")
+        version_locations = [script_location]
+        self.__config.set_main_option("script_location", script_location)
+        self.__config.set_main_option(
+            "version_locations", ",".join(version_locations)
+        )
         return self.__config
 
     @property
     def script_directory(self) -> ScriptDirectory:
         if isinstance(self.__script_directory, ScriptDirectory):
             return self.__script_directory
-        self.__script_directory = ScriptDirectory()
+        logger.debug(
+            f"config script_location:{self.config.get_main_option('script_location')}"
+        )
+        self.__script_directory = ScriptDirectory.from_config(self.config)
         return self.__script_directory
 
     @property
@@ -86,15 +92,18 @@ class Moonshine:
     def environment_context(self) -> EnvironmentContext:
         if isinstance(self.__environment_context, EnvironmentContext):
             return self.__environment_context
-        self.__environment_c__environment_contextontect = EnvironmentContext()
+        self.__environment_context = EnvironmentContext(
+            self.config, self.script_directory
+        )
         return self.__environment_context
 
     @property
+    @contextmanager
     def migration_context(self):
         with self.engine.connect() as conn:
             env = self.environment_context
             env.configure(connection=conn)
-            yield env
+            yield env.get_context()
 
     def run_migrations(self, fn, **kwargs):
         """Configure an Alembic :class:`~alembic.runtime.migration.MigrationContext` to run migrations for the given function.
@@ -115,12 +124,13 @@ class Moonshine:
             with env.begin_transaction():
                 env.run_migrations(**kwargs)
 
+    @property
     def current(self):
         """Get the list of current revisions."""
-
-        return self.script_directory.get_revisions(
-            self.migration_context.get_current_heads()
-        )
+        with self.migration_context as migration_context:
+            return self.script_directory.get_revisions(
+                migration_context.get_current_heads()
+            )
 
     def heads(self, resolve_dependencies=False):
         """Get the list of revisions that have no child revisions.
@@ -131,7 +141,9 @@ class Moonshine:
         if resolve_dependencies:
             return self.script_directory.get_revisions("heads")
 
-        return self.script_directory.get_revisions(self.script_directory.get_heads())
+        return self.script_directory.get_revisions(
+            self.script_directory.get_heads()
+        )
 
     def branches(self):
         """Get the list of revisions that have more than one next revision."""
@@ -142,28 +154,67 @@ class Moonshine:
             if revision.is_branch_point
         ]
 
-    def log(self, start="base", end="heads"):
-        """Get the list of revisions in the order they will run.
+    def history(
+        self, rev_range="base:heads", verbose=False, indicate_current=False
+    ):
+        """List changeset scripts in chronological order.
 
-        :param start: only get since this revision
-        :param end: only get until this revision
+        :param config: a :class:`.Config` instance.
+
+        :param rev_range: string revision range
+
+        :param verbose: output in verbose mode.
+
+        :param indicate_current: indicate current revision.
+
+        ..versionadded:: 0.9.9
+
         """
 
-        if start is None:
-            start = "base"
-        elif start == "current":
-            start = [r.revision for r in self.current()]
+        if rev_range is not None:
+            if ":" not in rev_range:
+                raise util.CommandError(
+                    "History range requires [start]:[end], "
+                    "[start]:, or :[end]"
+                )
+            base, head = rev_range.strip().split(":")
         else:
-            start = getattr(start, "revision", start)
+            base = head = None
 
-        if end is None:
-            end = "heads"
-        elif end == "current":
-            end = [r.revision for r in self.current()]
+        environment = (
+            util.asbool(self.config.get_main_option("revision_environment"))
+            or indicate_current
+        )
+
+        def _display_history(base, head, currents=()):
+
+            history = list()
+            for sc in self.script_directory.walk_revisions(
+                base=base or "base", head=head or "heads"
+            ):
+                if indicate_current:
+                    sc._db_current_indicator = sc.revision in currents
+                history.append(sc)
+
+            return history
+
+        def _display_history_w_current(base, head):
+            def _display_current_history(rev):
+                if head == "current":
+                    return _display_history(base, rev, rev)
+                elif base == "current":
+                    return _display_history(rev, head, rev)
+                else:
+                    return _display_history(base, head, rev)
+                return []
+
+            rev = self.current
+            return _display_current_history(rev)
+
+        if base == "current" or head == "current" or environment:
+            return _display_history_w_current(base, head)
         else:
-            end = getattr(end, "revision", end)
-
-        return list(self.script_directory.walk_revisions(start, end))
+            return _display_history(base, head)
 
     def stamp(self, target="heads"):
         """Set the current database revision without running migrations.
@@ -171,7 +222,9 @@ class Moonshine:
         :param target: revision to set to, default 'heads'
         """
 
-        target = "heads" if target is None else getattr(target, "revision", target)
+        target = (
+            "heads" if target is None else getattr(target, "revision", target)
+        )
 
         def do_stamp(revision, context):
             return self.script_directory._stamp_revs(target, revision)
@@ -185,7 +238,9 @@ class Moonshine:
         """
 
         revision = (
-            "heads" if revision is None else getattr(revision, "revision", revision)
+            "heads"
+            if revision is None
+            else getattr(revision, "revision", revision)
         )
         revision = str(revision)
 
